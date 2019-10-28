8480df580711fff105922688c2f709f011c09bab
Vinod Mandloi <vmandloye@gmail.com>
2019-10-28 13:53:18 +0530

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,11 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import {  MatCardModule } from '@angular/material/card';
 import {  MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
+
 
 import {  AppRoutingModule } from './app-routing/app-routing.module'
 
@@ -52,7 +57,11 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
   ],
   providers: [
     DishService,
